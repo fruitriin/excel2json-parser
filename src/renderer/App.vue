78eef647848a8ad04ba323b60d304899448b45bf
中島凜 <nassii74@gmail.com>
2018-08-06 18:23:47 +0900

    設定の画面構成をほげほげ

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,9 +1,25 @@
 <template>
   <div id="app">
-    <div class="file-drop-area" v-if="file">
+    <div class="file-drop-area" v-if="!file">
       Excelファイルをドラッグ・アンド・ドロップしてください。
     </div>
+    <div v-else>
+    </div>
 
+    <div class="sheetTabs">
+      シート名(タブ)
+    </div>
+    <div class="sheetConfigs">
+      シート別詳細設定
+
+      出力列:
+
+      ヘッダー行調整:
+      データ行調整:
+
+      データのキー:
+      
+    </div>
 
 
   </div>
@@ -12,10 +28,12 @@
 <script>
   import LandingPage from '@/components/LandingPage';
 
-  const sheetConf = {
-    headerRowOffset: 0,
+  const sheetConfDefault = {
+      willOutput: true,
+      headerRowOffset: 0,
       dataRowOffset: 0,
-      
+      targetColNames: [],
+      preview: false
   }
 
   export default {
