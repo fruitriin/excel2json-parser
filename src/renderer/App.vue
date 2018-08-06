ae434bf84a9474d473c56174b745f1f69c858d5e
中島凜 <nassii74@gmail.com>
2018-08-06 18:11:14 +0900

    えれくとろーん

---
+++ src/renderer/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <div class="file-drop-area" v-if="file">
+      Excelファイルをドラッグ・アンド・ドロップしてください。
+    </div>
+
+
+
+  </div>
+</template>
+
+<script>
+  import LandingPage from '@/components/LandingPage';
+
+  const sheetConf = {
+    headerRowOffset: 0,
+      dataRowOffset: 0,
+      
+  }
+
+  export default {
+      name: 'excel2json-parser',
+      data: function(){
+        return {
+            file: {},
+            rawData: {},
+            config: {
+
+            },
+        }
+      },
+      computed:{
+        previewData(){
+
+        }
+      },
+      methods:{
+          openFile(){
+
+          },
+          outputJson(){
+
+          },
+          saveConfig(){
+
+          },
+      }
+
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
---
+++ src/renderer/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import App from './App';
+
+
+Vue.use(BootstrapVue);
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  template: '<App/>',
+}).$mount('#app');
