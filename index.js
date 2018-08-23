771343b8c3b6ae82307f40597e4778b9e95bf2e6
中島凜 <nassii74@gmail.com>
2018-08-23 14:23:29 +0900

    アイテムをパース

--- index.js
+++ index.js
@@ -23,7 +23,42 @@ const configs = {
                 "Spec", "ShortDesc", "Next"
 
             ]
+        },
+        "Item": {
+            "targetColumns" : [
+                "Group","Type","Lv","Name",
+                "Main","Sub","Mc","Sc","Dam","Arm","LD","Cap","Holy",
+                "AM","Size","Str","Cha","Re","Two","Fla","Head","Res","Bon",
+                "NbrA","AoE","Lin","Att","Def","Ran","Pre","Amm","UW","Len",
+                "HP","BP","SP","Ade","Par","Enc","MMp","Mag","OnD","OnH",
+                "OnA","FR","SR","CR","PR","AR","IP","Inv","PF","Awe","AA",
+                "Fear","He","Ch","FS","AS","OC","PB","Pet","DR",
+                "Bless","Ber","GB","Tra","DV","Reg","Rei","Ste","invis","Ass",
+                "Sed","Rea","Her","Inq","AdS","DoS","GoM","Ins","TM","BM","Co",
+                "Uc","Pill","SupB","WB","Sail","Heal","Mas","Eth","Swi",
+                "Qui","Enl","StP","Luck","TF","FL","Curse","Taint","Dise","Eye","Che",
+                "Fee","Insa","Sie","Fly","SI","Flo","FSu","MSu","SSu","WSu","SnM","Swim",
+                "BS","ReB","DM","IL","ACB","ForB","MaS","MaR","Alc","CoM","CoS","Res",
+                "F","A","W","E","S","D","N","B","H","FC","GG","TG","Spec"]
+        },
+        "NationalItem" : {
+            "targetColumns" : [
+                "Group","Type","Lv","Name",
+                "Main","Sub","Mc","Sc","Dam","Arm","LD","Cap","Holy",
+                "AM","Size","Str","Cha","Re","Two","Fla","Head","Res","Bon",
+                "NbrA","AoE","Lin","Att","Def","Ran","Pre","Amm","UW","Len",
+                "HP","BP","SP","Ade","Par","Enc","MMp","Mag","OnD","OnH",
+                "OnA","FR","SR","CR","PR","AR","IP","Inv","PF","Awe","AA",
+                "Fear","He","Ch","FS","AS","OC","PB","Pet","DR",
+                "Bless","Ber","GB","Tra","DV","Reg","Rei","Ste","invis","Ass",
+                "Sed","Rea","Her","Inq","AdS","DoS","GoM","Ins","TM","BM","Co",
+                "Uc","Pill","SupB","WB","Sail","Heal","Mas","Eth","Swi",
+                "Qui","Enl","StP","Luck","TF","FL","Curse","Taint","Dise","Eye","Che",
+                "Fee","Insa","Sie","Fly","SI","Flo","FSu","MSu","SSu","WSu","SnM","Swim",
+                "BS","ReB","DM","IL","ACB","ForB","MaS","MaR","Alc","CoM","CoS","Res",
+                "F","A","W","E","S","D","N","B","H","FC","GG","TG","Spec"]
         }
+
 
     }
 };
@@ -52,6 +87,7 @@ for(let sheet of sheets){
 
         let newrow = {};
         for(let col of index){
+            console.log(col);
             newrow[col.name] = row[col.index] !== undefined ? row[col.index] : null;
         }
         newdata.push(newrow);
